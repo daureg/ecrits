f867e737d86854dd076af4d83c5673fbfa4035a2
Géraud Le Falher <daureg@gmail.com>
2011-04-24 18:48:17 +0200

    ajout de la nouvelle Sélène irradiée

---
+++ info/selene.mk
@@ -0,0 +1,9 @@
+NAME			=	selene
+FULL_TITLE		=	"Sélène irradiée"
+DESC			=	"Zohel enquête sur la disparition mystérieuse de Jane de la cage dans laquelle il vit."
+KEYWORD			=	"nouvelle,lune,radiation,métamorphose,singe"
+DATE            	=   	"Février 2001"
+ROOT_LEVEL		=	"2"
+PDF_VIEWER		=	epdfview
+VERSION			=	0.1
+
