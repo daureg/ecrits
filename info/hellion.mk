2eadba6647597aaee44db61322159cf7d8c6dc35
Géraud Le Falher <daureg@gmail.com>
2011-04-24 21:31:54 +0200

    ajout de 'hellion', le début d'une tragédie en alexandrin

---
+++ info/hellion.mk
@@ -0,0 +1,9 @@
+NAME			=	hellion
+FULL_TITLE		=	"Hellion"
+DESC			=	"Le début d'une tragédie en alexandrin."
+KEYWORD			=	"tragédie,alexandrin"
+DATE            	=   	"Début 2009"
+ROOT_LEVEL		=	"2"
+PDF_VIEWER		=	epdfview
+VERSION			=	0.1
+
