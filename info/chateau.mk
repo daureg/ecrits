f7804ef3121db98d4ca9b51fc6c432f8915912b9
Géraud Le Falher <daureg@gmail.com>
2011-04-24 21:32:22 +0200

    ajout de 'chateau', aucune idée de ce que c'est sensé être.

---
+++ info/chateau.mk
@@ -0,0 +1,9 @@
+NAME			=	chateau
+FULL_TITLE		=	"Château"
+DESC			=	"Des châteaux dans les cieux"
+KEYWORD			=	"nouvelle,château,ciel"
+DATE            	=   	"date inconnue"
+ROOT_LEVEL		=	"2"
+PDF_VIEWER		=	epdfview
+VERSION			=	0.1
+
