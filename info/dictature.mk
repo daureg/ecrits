cdc3007abb3a8e00a84cbd7a5c666cff4b0b2e8d
Géraud Le Falher <daureg@gmail.com>
2011-05-02 13:43:41 +0200

    problème de date

--- info/dictature.mk
+++ info/dictature.mk
@@ -1,7 +1,7 @@
 FULL_TITLE		=	"Politique spectacle"
 DESC			=	"Une planète où les dictateurs les plus farfelus sont élus à l'issue de tests étranges"
 KEYWORD			=	"nouvelle,dictature,science-fiction"
-DATE            	=   	"Octobre 2011"
+DATE            	=   	"Octobre 2010"
 ROOT_LEVEL		=	"2"
 PDF_VIEWER		=	epdfview
 VERSION			=	0.1
