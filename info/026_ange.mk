70717d17e941ac7b29ec1cf2fd9c5a511be8a119
Géraud Le Falher <daureg@gmail.com>
2012-02-23 18:56:06 +0100

    pour les annales, la première version de 'ange'

---
+++ info/026_ange.mk
@@ -0,0 +1,8 @@
+FULL_TITLE		=	"Le Python de Delphes"
+DESC			=	"Un homme de ménage se lie d'amitié avec un ordinateur capable de prédir l'avenir."
+KEYWORD			=	"nouvelle,machine learning,saut de l'ange,catastrophe"
+DATE            	=   	"Février 2012"
+ROOT_LEVEL		=	"2"
+PDF_VIEWER		=	epdfview
+VERSION			=	1.0
+FINI			=	1
