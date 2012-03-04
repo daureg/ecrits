514d87c9796f8b5164775899a06c433daa7f5dfc
Géraud Le Falher <daureg@gmail.com>
2012-03-04 22:39:16 +0100

    la numérotation est plus générale

--- info/classe.py
+++ info/classe.py
@@ -1,8 +1,10 @@
 # vim: set fileencoding=utf-8
 import sys
+OFFSET = 0
 with open("chronologie") as f:
     l = [i.split(';')[0].strip() for i in f.readlines()]
 for i,v in enumerate(l):
+    v += OFFSET
     dest = "{:03d}_{}".format(i+1,v)
     print("git mv {f}.nv {d}.nv\ngit mv info/{f}.mk \
 info/{d}.mk".format(f=v,d=dest))
