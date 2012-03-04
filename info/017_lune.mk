d76a6fee7b368e9a36299d638c7adb90ca62b544
Géraud Le Falher <daureg@gmail.com>
2012-03-04 22:38:57 +0100

    typo

--- info/017_lune.mk
+++ info/017_lune.mk
@@ -1,7 +1,7 @@
 FULL_TITLE		=	"Et avec un peu de volonté ?"
 DESC			=	"Un monde où les gens peuvent faire ce qu'ils veulent en se concentrant suffisament. Une femme, un nordique et un cheval qui parle se battent contre un parti unique"
 KEYWORD			=	"nouvelle,lune,imagination,dictature"
-DATE            	=   	"Novemebre 2008"
+DATE            	=   	"Novembre 2008"
 ROOT_LEVEL		=	"2"
 PDF_VIEWER		=	epdfview
 VERSION			=	0.1
