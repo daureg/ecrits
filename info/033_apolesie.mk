39adad8a1ee259106618fd57e9a59882b0ba8428
Géraud Le Falher <daureg@gmail.com>
2012-08-29 19:49:27 +0200

    ajout d'un nouveau texte

---
+++ info/033_apolesie.mk
@@ -0,0 +1,9 @@
+FULL_TITLE		=	"Juste un peu d'or"
+DESC			=	"Une aventure d'un brigand peu prometteur."
+KEYWORD			=	"nouvelle,fantasy,crime,or"
+DATE            	=   	"2012"
+ROOT_LEVEL		=	"2"
+PDF_VIEWER		=	epdfview
+VERSION			=	1.0
+FINI			=	1
+
