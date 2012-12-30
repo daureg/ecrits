115c6ca4b54c1e0e3a3cf8a072b8ccd733ad7347
Graud Le Falher <Geraud.Le-Falher@eleves.ec-nantes.fr>
2012-12-30 18:07:16 +0200

    ajout de deux autres debuts de texte

---
+++ info/034_tete.mk
@@ -0,0 +1,8 @@
+FULL_TITLE		=	"Nouvelles technologies"
+DESC			=	""
+KEYWORD			=	"nouvelle"
+DATE            	=   	"Septembre 2012"
+ROOT_LEVEL		=	"2"
+PDF_VIEWER		=	epdfview
+VERSION			=	0.1
+FINI			=	0
---
+++ info/035_discret.mk
@@ -0,0 +1,8 @@
+FULL_TITLE		=	"Discrets ?"
+DESC			=	""
+KEYWORD			=	"nouvelle"
+DATE            	=   	"Décembre 2012"
+ROOT_LEVEL		=	"2"
+PDF_VIEWER		=	epdfview
+VERSION			=	0.1
+FINI			=	0
