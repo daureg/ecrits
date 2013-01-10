5e5f30cb14a37d8494130c0e77bc464c25f54c9a
Graud Le Falher <Geraud.Le-Falher@eleves.ec-nantes.fr>
2013-01-10 11:53:37 +0200

    suite et fin de 035_discret

--- info/035_discret.mk
+++ info/035_discret.mk
@@ -1,7 +1,7 @@
-FULL_TITLE		=	"Discrets ?"
-DESC			=	""
-KEYWORD			=	"nouvelle"
-DATE            	=   	"Décembre 2012"
+FULL_TITLE		=	"Une sorte d\'ami"
+DESC			=	"Un groupe de dissident s'invite à la fête d'un général peu fréquentable"
+KEYWORD			=	"nouvelle,infiltration,trahison"
+DATE            	=   	"Janvier 2013"
 ROOT_LEVEL		=	"2"
 PDF_VIEWER		=	epdfview
 VERSION			=	0.1
