e666c131c04f1170e58773a7a3182b9ea10a8ff7
Graud Le Falher <Geraud.Le-Falher@eleves.ec-nantes.fr>
2013-02-02 01:03:54 +0200

    premier jet pour la nouvelle jet d'encre 2013.

---
+++ info/036_neige.mk
@@ -0,0 +1,8 @@
+FULL_TITLE		=	"Sous la neige les cactus"
+DESC			=	"Une tueuse à gage fait face aux restrictions du marché noir"
+KEYWORD			=	"nouvelle,marché noire,tueuse"
+DATE            	=   	"Février 2013"
+ROOT_LEVEL		=	"2"
+PDF_VIEWER		=	epdfview
+VERSION			=	0.1
+FINI			=	0
